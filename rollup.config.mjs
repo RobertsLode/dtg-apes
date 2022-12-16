15f8abf650718160aa61a2f03c56d0bfc1ab5ab3
RobertsLode <robislode@gmail.com>
2022-12-16 14:17:12 +0200

    a

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,10 +8,10 @@ export default [
     {
         input: './src/index.js',
         output: [
-            // {
-            //     file: 'dist/index.js',
-            //     format: 'cjs',
-            // },
+            {
+                file: 'dist/index.js',
+                format: 'cjs',
+            },
             {
                 file: 'dist/index.es.js',
                 format: 'es',
