e2cfb58faabf3d6bf8943f34f288849d7d361df8
RobertsLode <robislode@gmail.com>
2022-12-23 12:36:37 +0200

    a

--- src/components/malcens/components/ClearButton/ClearButton.jsx
+++ src/components/malcens/components/ClearButton/ClearButton.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
